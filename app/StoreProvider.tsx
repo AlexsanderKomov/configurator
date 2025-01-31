2f04d8fe201a262faa1cdfdd31ea2f4835a90987
Комов Александр <akomov@delc.lan>
2025-01-31 09:08:36 +0300

    add store

---
+++ app/StoreProvider.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useRef } from "react";
+import { Provider } from "react-redux";
+import { makeStore, AppStore } from "@/lib/store";
+import { initializeCount } from "@/lib/api";
+
+export default function StoreProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const storeRef = useRef<AppStore | null>(null);
+  if (!storeRef.current) {
+    storeRef.current = makeStore();
+    storeRef.current.dispatch(initializeCount(count));
+  }
+
+  return <Provider store={storeRef.current}>{children} </Provider>;
+}
