d9de769dd2be522c0986274d2e8fdbd2cbf6e6e4
Комов Александр <akomov@delc.lan>
2025-02-14 12:11:47 +0300

    перенес содежражание readExcel.jsx в ReadExcel index.tsx

--- components/layout/Main/ReadExcel/index.tsx
+++ components/layout/Main/ReadExcel/index.tsx
@@ -1,13 +1,34 @@
-import { handleFileReader } from "@/lib/helpers/readExcel";
+import { ChangeEvent, FormEvent, useState } from "react";
+import * as XLSX from "xlsx";
+
+import { IUseState } from "./interface";
 
 const ReadExcel = () => {
+  const [data, setData] = useState<IUseState[]>([]);
+
+  function handleFileReader(e: ChangeEvent<HTMLInputElement>) {
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      const data = e.target.result;
+      const workbook = XLSX.readFile(data, { type: "binary" });
+      const sheetName = workbook.SheetNames[0];
+      const firstSheet = workbook.Sheets[sheetName];
+      const firstSheetData = XLSX.utils.sheet_to_json(firstSheet);
+
+      // console.log(firstSheetData);
+      setData(firstSheetData);
+    };
+    reader.readAsArrayBuffer(e.target[0].files[0]);
+  }
+
   return (
     <form
-      onSubmit={(e) => {
+      onSubmit={(e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         console.log("File uploaded");
         handleFileReader(e);
-        console.log(e);
+        console.log(data);
       }}
     >
       <input type="file" accept=".xls, .xlsx" />
---
+++ components/layout/Main/ReadExcel/interface.ts
@@ -0,0 +1,5 @@
+export type IUseState = {
+  manufacturer: string;
+  name: string;
+  article: string;
+}[];
